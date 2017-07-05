25d35f16a29fd453d21a866062e4486094a2280b
hexudong <zqkl123456>
2017-07-05 15:16:32 +0800

    Initial commit

---
+++ src/main/java/cn/slimsmart/rabbitmq/demo/helloword/Send.java
@@ -0,0 +1,43 @@
+package cn.slimsmart.rabbitmq.demo.helloword;
+
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+public class Send {
+    //消息队列名称
+    private final static String QUEUE_NAME = "helloword";
+
+    public static void main(String[] args) throws Exception {
+        /**
+         * 创建连接连接到MabbitMQ
+         */
+        ConnectionFactory factory = new ConnectionFactory();
+        //设置MabbitMQ所在主机ip或者主机名
+        factory.setHost("192.168.1.5");
+        //指定用户 密码
+        factory.setUsername("hxd");
+        factory.setPassword("hxd");
+        factory.setVirtualHost("test");
+        //指定端口
+        factory.setPort(AMQP.PROTOCOL.PORT);
+        //创建一个连接
+        Connection connection = factory.newConnection();
+        //创建一个频道
+        Channel channel = connection.createChannel();
+        //指定一个队列
+        //channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        // channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        channel.queueDeclare(QUEUE_NAME, true, false, false, null);
+        //发送的消息
+        String message = "hello world!";
+        //往队列中发出一条消息
+        channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+        System.out.println("Sent Message：'" + message + "'");
+        //关闭频道和连接
+        channel.close();
+        connection.close();
+    }
+
+}
